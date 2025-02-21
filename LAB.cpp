98d2b79a426b388753bc31e52ba2c39eefb5fea7
Kartoshka <kulcickijo@gmail.com>
2025-02-21 10:33:03 +0200

    +file

---
+++ LAB.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    cout << "Hello, world!" << endl;
+    cout << "2 + 3 = " << add(2, 3) << endl;
+    return 0;
+}
+
